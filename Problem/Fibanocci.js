34626d499dac5c078066ca34c66c187e66e270f7
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-08-23 14:03:42 +0800

    Create Fibanocci.js

---
+++ Problem/Fibanocci.js
@@ -0,0 +1,11 @@
+const fibanocci = (n) => {
+  const fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+};
+
+console.log(fibanocci(2)); // [0, 1]
+console.log(fibanocci(3)); // [0, 1, 1]
+console.log(fibanocci(7)); // [0, 1, 1, 2, 3, 5, 8]
