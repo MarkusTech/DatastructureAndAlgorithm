e391e8def807a92d7800c42e82c2df1c6098a905
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-08-23 15:40:55 +0800

    Create recursiveFactorial.js

---
+++ Recursive Algorithm/recursiveFactorial.js
@@ -0,0 +1,10 @@
+const recursiveFactorial = (n) => {
+  if (n === 0) {
+    return 1;
+  }
+  return n * recursiveFactorial(n - 1);
+};
+
+console.log(recursiveFactorial(0)); // 1
+console.log(recursiveFactorial(1)); // 1
+console.log(recursiveFactorial(5)); // 120
