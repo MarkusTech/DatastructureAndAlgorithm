5d72fee79eb33454acd4abe9da2a3a0a49d305a4
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-08-23 15:51:14 +0800

    Create linearSearch.js

---
+++ Search Algorithm/linearSearch.js
@@ -0,0 +1,16 @@
+/*
+    PROBLEM - Given an array of 'n' element and a target element 't', find the index 
+    of 't' in the array. Return -1 if the target elemetn is not found
+*/
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(linearSearch([-5, 2, 10, 4, 6], 10)); // 2
+console.log(linearSearch([-5, 2, 10, 4, 6], 6)); // 4
+console.log(linearSearch([-5, 2, 10, 4, 6], 20)); // -1
