8afc91152894d67ba53cca23114a6db3b9e857c8
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-08-23 16:18:36 +0800

    Update binarySearch.js

--- Search Algorithm/binarySearch.js
+++ Search Algorithm/binarySearch.js
@@ -1,1 +1,26 @@
-function binarySearch() {}
+/*
+    PROBLEM - Given a sorted array of 'n' elements and a target element 't', find 
+    the index of 't' in the array. return -1 if the elements is not found
+*/
+
+function binarySearch(arr, target) {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (target === arr[middleIndex]) {
+      return middleIndex;
+    }
+    if (target < arr[middleIndex]) {
+      rightIndex = middleIndex - 1;
+    } else {
+      leftIndex = middleIndex + 1;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch([-5, 2, 4, 6, 10], 10)); // 4
+console.log(binarySearch([-5, 2, 4, 6, 10], 6)); // 3
+console.log(binarySearch([-5, 2, 4, 6, 10], 20)); // -1
