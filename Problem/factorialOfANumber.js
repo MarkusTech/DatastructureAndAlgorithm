27c79dc05ee7579bb7ea540ca302882b42f136b6
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-08-23 14:21:07 +0800

    Update factorialOfANumber.js

---
+++ Problem/factorialOfANumber.js
@@ -0,0 +1,10 @@
+const factorial = (n) => {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
+};
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(5)); // 120
