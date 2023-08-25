d59bfaef8d1428419f3e2b26e446559cf3455c3c
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-08-25 09:27:22 +0800

    Update insertionSort.js

---
+++ Sorting Algorithm/insertionSort.js
@@ -0,0 +1,16 @@
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let numberToInsert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numberToInsert) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = numberToInsert;
+  }
+};
+
+// output
+const arr = [8, 20, -2, 4, -6];
+insertionSort(arr);
+console.log(arr);
