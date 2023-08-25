5f689328680f0f39a686bd3177626b5de56e0d77
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-08-25 13:26:07 +0800

    Update ClimbingStaircase.js

---
+++ Misc Problem/ClimbingStaircase.js
@@ -0,0 +1,14 @@
+const climbingStaircase = (n) => {
+  const noOfWays = [1, 2];
+  for (let i = 2; i <= n; i++) {
+    noOfWays[i] = noOfWays[i - 1] + noOfWays[i - 2];
+  }
+
+  return noOfWays[n - 1];
+};
+
+console.log(climbingStaircase(1)); // 1
+console.log(climbingStaircase(2)); // 2
+console.log(climbingStaircase(3)); // 3
+console.log(climbingStaircase(4)); // 5
+console.log(climbingStaircase(5)); // 8
