7e5006dfc845c4a85a240589a5ae6a2a08e8aeb6
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-09-08 09:51:35 +0800

    Find the Largest Number

---
+++ nutnull/findTheLargestNumber.js
@@ -0,0 +1,17 @@
+const numbers = [1, 2, 3, 4, 5, 6];
+
+// Option 1
+console.log(Math.max(...numbers));
+
+// Option 2
+const largestNumber = (values) => {
+  let highest = 0;
+  for (let i = 0; i < values.length; i += 1) {
+    if (values[i] > highest) {
+      highest = values[i];
+    }
+  }
+  return highest;
+};
+
+console.log(largestNumber(numbers));
