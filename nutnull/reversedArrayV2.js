7bd62a9c7d217f3ed26cd631ac1e4afd7954009d
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-09-09 11:35:06 +0800

    Update reversedArrayV2.js

--- nutnull/reversedArrayV2.js
+++ nutnull/reversedArrayV2.js
@@ -1,3 +1,11 @@
+/* Algorithm
+  1. create a storage of the reversedArray
+  2. iterate the values of the array
+  3. store the iterated value in reversedArray in every iteration but the iteration is iteration--
+  4. if the iteration is done print the reverseArray values
+  5. done
+*/
+
 const reverse = (arr) => {
   let a = [];
   for (let i = 0; i < arr.length; i++) {
@@ -8,4 +16,4 @@ const reverse = (arr) => {
 
 console.log(reverse([1, 2, 3, 4]));
 console.log(reverse([9, 9, 2, 3, 4]));
-console.log(reverse([]));
+console.log(reverse([4, 3, 2, 1]));
