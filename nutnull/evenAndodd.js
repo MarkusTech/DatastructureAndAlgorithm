f82c40ef3da76ea5c9f9097ca5b621af1217d243
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-09-09 11:04:27 +0800

    Update evenAndodd.js

--- nutnull/evenAndodd.js
+++ nutnull/evenAndodd.js
@@ -1,3 +1,11 @@
+/* Algorithm
+  1. identify if the input number is %2 and equal to 0
+  2. if equal to zero then return true
+  3. else must be false
+  4. false becase if input %3 == 0
+  5. if your input is 2 then is not == 0 thats why it will return false
+*/
+
 const evenOrOdd = (n) => {
   if (n % 2 == 0) {
     console.log(true);
