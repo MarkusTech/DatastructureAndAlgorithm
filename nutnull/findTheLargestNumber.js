ae578ca94702aec5c38c67abcc2e5ae541ac6bc9
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-09-09 11:10:42 +0800

    Update findTheLargestNumber.js

--- nutnull/findTheLargestNumber.js
+++ nutnull/findTheLargestNumber.js
@@ -1,9 +1,21 @@
 const numbers = [1, 2, 3, 4, 5, 6];
 
 // Option 1
+/* Algorithm
+  1. print using Math module max then use the spread operator built in javascript
+*/
 console.log(Math.max(...numbers));
 
 // Option 2
+/* Algorithm
+  1. create a variable for the storage of the largest number
+  2. iterate the values in array
+  3. set a condition if the iterated value is greated than the stored 
+      value of the created variable
+  4. if the condition is meet then the variable will record the iterated value
+  5. if done iterating all of the values in array Print the variable
+  6. done!
+*/
 const largestNumber = (values) => {
   let highest = 0;
   for (let i = 0; i < values.length; i += 1) {
